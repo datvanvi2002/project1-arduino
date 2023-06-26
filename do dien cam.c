91656266391dc51312343fe9e383ba6ea34bf2f2
datvanvi2002 <84333163+datvanvi2002@users.noreply.github.com>
2023-06-26 23:20:29 +0700

    Update do dien cam.c

--- do dien cam.c
+++ do dien cam.c
@@ -1,35 +1,86 @@
-
-#include <Wire.h>
+// Bai code duoc thuc hien boi sinh vien VI VAN DAT khoa dien -SEEE-HUST
 #include <LiquidCrystal.h> // Thu vien lcd
   LiquidCrystal lcd(7, 6,5,4,3, 2); //thu tu
-  double pulse, tanso, cap, diencam, diencam_mH;
-
+  double pulse,pulse1, tanso, cap, diencam, diencam_mH,diencam_uH;
+  int relayPin= 0;
 void setup(){
+
 	lcd.begin(16,2); //bat man hinh lcd 1602
-	
+
+	pinMode(relayPin, OUTPUT);
 	pinMode(11, INPUT); //doc tin hieu cua bo so sanh
-	pinMode(13, OUTPUT);//phat tin hieu cho mach LC dao dong
+	pinMode(13, OUTPUT);//phat tin hieu cho machLC dao dong
 
 }
 void loop(){
+
+	digitalWrite(relayPin, LOW);
+
 	digitalWrite(13, HIGH);
-	delay(5);                       //phat tin hieu nap dien cho mach LC trong 5ms sau do dung
+	delay(10);                     
 	digitalWrite(13, LOW);
-	delayMicroseconds(100);         //dung chương trinh trong thoi gian 100 micro giây, de mach LC dao dong on dinh
-	pulse = pulseIn(11, HIGH, 5000);  //tra ve thoi gian micro giay de thay doi trang thai L-H 1s=10^6, tra ve 0 neu het thoi gian cho phep
-    if(pulse > 0.1){ //neu thoi gian > 0.1
-		cap = 2.E-6; // Gia tri dien dung, F 
-		tanso = 1.E6/(2*pulse); // tinh tan so 
-		diencam = 1/(cap*tanso*tanso*4.*3.14159*3.14159); // tinh dien cam dua tren cong thuc
-		diencam=diencam*1.E6;
-        diencam_mH = diencam/1000; // đoi sang đơn vi mH
-        //LCD hien thi
-        lcd.setCursor(0, 0); // Hàng 0, cot 0
-        lcd.print("Gia tri dien cam");
-        lcd.setCursor(0, 1); //Hàng 1, cot 0
-        lcd.print(diencam_mH);
-        lcd.setCursor(14, 1);//Hàng 1 cot 14
-        lcd.print("mH");
-        delay(10);
-    }
+	delayMicroseconds(100);        
+	pulse = pulseIn(11, HIGH,180000000);  
+	lcd.clear();
+	
+
+ if(pulse > 0.1){  
+	if( pulse <141){	
+		cap = 2.E-6; 
+		tanso = 1.E6/(2*pulse); 
+		diencam = 1/(cap*tanso*tanso*4.*3.14159*3.14159);//F
+		diencam_uH=diencam*1.E6;
+		
+		lcd.clear();
+		lcd.setCursor(0, 0);
+		lcd.print("Gia tri dien cam");
+
+		lcd.setCursor(0, 1); 
+		lcd.print(diencam_uH,1);
+		lcd.setCursor(14, 1);
+		lcd.print("uH");
+		delay(500);
+	}
+	while(pulse>140){
+		digitalWrite(relayPin, HIGH);
+		digitalWrite(13, HIGH);
+		delay(10);                       
+		digitalWrite(13, LOW);
+		delayMicroseconds(100);         
+		pulse1 = pulseIn(11, HIGH,180000000);
+
+		lcd.clear();
+		lcd.print(pulse1);delay(1000);
+		cap = 2.E-3; 
+		tanso = 1.E6/(2*pulse1);
+		diencam = 1/(cap*tanso*tanso*4.*3.14159*3.14159); 
+		diencam_mH = diencam*1000; 
+
+		lcd.setCursor(0, 0); 
+		lcd.clear();
+		lcd.print("Gia tri dien cam");
+
+		if(diencam_mH <999){
+			lcd.setCursor(0, 1); 
+			lcd.print(diencam_mH,1);
+			lcd.setCursor(14, 1);
+			lcd.print("mH");
+			delay(500);
+		}else{
+			lcd.setCursor(0, 1); 
+			lcd.print(diencam,1);
+			lcd.setCursor(14, 1);
+			lcd.print("H");
+			delay(500);
+		}
+		if(pulse1<4444){
+			break;
+		}
+	}
+ }else{
+	lcd.print(pulse);
+	delay(1000);
+	lcd.print(" ERROR ");
+ }
 }
+//update 18-06-2023
